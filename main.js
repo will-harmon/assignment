d141f5544acb60c6b43d9ddc43c3433f97c13698
Will Harmon <willcharmon@gmail.com>
2019-06-13 20:00:53 -0400

    commit

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function add() {
-  document.getElementById("buyButton").innerHTML = "ADDED TO CART!";
-}
+var items = 0;
+
+$("button").click(function () {
+  items= items + 1;
+  $(".cart-items").text(items);
+
+  $("button").text("ADDED TO CART!");
+  setTimeout(function () {
+    $("button").text("Buy Now");
+  }, 1000);
+});
